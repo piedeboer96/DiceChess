43606d6ef11f410d46ef0eb59237b248249a9787
eden <e.edenrochman@student.maastrichtuniversity.nl>
2022-10-13 14:12:29 +0200

    Castle comments
    Comments about castle

--- src/test/java/ai/test/t11/CastleRookPointOfView.java
+++ src/test/java/ai/test/t11/CastleRookPointOfView.java
@@ -11,7 +11,14 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+/*
 
+    Your king can not have moved- Once your king moves, you can no longer castle, even if you move the king back to the starting square. Many strategies involve forcing the opponent’s king to move just for this reason.
+    Your rook can not have moved- If you move your rook, you can’t castle on that side anymore. Both the king and the rook you are castling with can’t have moved.
+    Your king can NOT be in check- Though castling often looks like an appealing escape, you can’t castle while you are in check! Once you are out of check, then you can castle. Unlike moving, being checked does not remove the ability to castle later.
+    Your king can not pass through check- If any square the king moves over or moves onto would put you in check, you can’t castle. You’ll have to get rid of that pesky attacking piece first!
+
+ */
 public class CastleRookPointOfView {
 
 	@Test
