9345e1b909d1cfd8c7d5fea37de1bf8f5c99f0b7
Valentina Cadena <v.cadenafajardo@student.maastrichtuniversity.nl>
2022-10-13 17:20:49 +0200

    promotion pawn test

--- src/test/java/ai/test/t7/PromotionPawnTest.java
+++ src/test/java/ai/test/t7/PromotionPawnTest.java
@@ -19,20 +19,31 @@ public class PromotionPawnTest {
     public void promotionPawnTest() {
 
         //Position that allows for white and black to promote after a valid roll
-        String startPos =  "8/k5P1/8/8/8/2K5/6p1/8 w - - 0 1";
+        String startPos =  "8/k5P1/8/8/8/2K5/6p1/8 b - - 0 1";
 
+        //Test for black
+        testTeam(false, startPos);
+        //Test for white
+        testTeam(true, startPos);
+    }
+
+    public void testTeam(boolean isNextPlayer, String startPos) {
         // Creating a new match
         ChessMatch match = new ChessMatch(startPos);
-        int nextPlayer = match.nextPlayer();
+        int player;
+
+        if (isNextPlayer) { player = match.nextPlayer(); }
+        else { player = match.getPlayer(); }
+
         EasyRuleEngine dumyRuleEngine = new EasyRuleEngine(match, 'p','p');
 
-        List<IChessMove> moves = match.legalMovesOf(nextPlayer);
+        List<IChessMove> moves = match.legalMovesOf(player);
         List<IChessMove> splitMoves = Utils.splitMoves(moves);
 
         System.out.println("All Legal moves ");
-        
+
         for (IChessMove mv : splitMoves) {
-            if (mv.owner().toFen() == 'p')
+            if (mv.owner().toFen() == 'p' || mv.owner().toFen() == 'x')
                 System.out.println(mv);
         }
 
@@ -41,10 +52,15 @@ public class PromotionPawnTest {
 
         dumyRuleEngine.play();
         ChessMove bestMove = dumyRuleEngine.getBestMove();
-        System.out.println("bestMove-->" + bestMove);
 
-        IChessboardSquare destination = bestMove.possibilities().get(0);
-        assertTrue(destination.rank() == 1 && destination.file() == 7);
-
+        if (player == 0) {
+            System.out.println("black bestMove-->" + bestMove);
+            IChessboardSquare destinationBlack = bestMove.possibilities().get(0);
+            assertTrue(destinationBlack.rank() == 7 && destinationBlack.file() == 6);
+        } else {
+            System.out.println("white bestMove-->" + bestMove);
+            IChessboardSquare destinationWhite = bestMove.possibilities().get(0);
+            assertTrue(destinationWhite.rank() == 0 && destinationWhite.file() == 6);
+        }
     }
 }
