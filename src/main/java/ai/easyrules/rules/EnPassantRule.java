4edbd590ef3a46ce187a5b3d4124af25877eee52
Saman Sarandib <samans@live.nl>
2022-10-14 13:22:29 +0200

    adding chessmatch

--- src/main/java/ai/easyrules/rules/EnPassantRule.java
+++ src/main/java/ai/easyrules/rules/EnPassantRule.java
@@ -3,6 +3,8 @@ package ai.easyrules.rules;
 import static ai.easyrules.rules.ruleUtils.checkColumnForPawnFromScratch;
 import static ai.easyrules.rules.ruleUtils.checkEnpassant;
 
+import chess.ChessMatch;
+import chess.utility.ChessMove;
 import org.jeasy.rules.annotation.Action;
 import org.jeasy.rules.annotation.Condition;
 import org.jeasy.rules.annotation.Fact;
@@ -17,6 +19,7 @@ import chess.utility.ChessboardSquare;
 @Rule(name = EnPassantRule.NAME, description = EnPassantRule.DESCRIPTION, priority = 10)            //set priority
 public class EnPassantRule extends ABaseRule
 {
+    ChessMatch match =  new ChessMatch();
     int file;
     int team;
     final static String DESCRIPTION = "enpassant move";
@@ -47,7 +50,7 @@ public class EnPassantRule extends ABaseRule
     @Action(order = 1)
     public void makeEnpassant(@Fact(LFacts.CHESSMOVE) IChessMove chessMove)
     {
-        if(checkColumnForPawnFromScratch(file,team , ""))             //set state fen string
+        if(checkColumnForPawnFromScratch(file,team , (ChessMatch)chessMove.toFen()))             //set state fen string
         {
             score = 75;
         }
