68a7ed829d4967cc53452243e687e8fde1381312
edenrochman <e.edenrochman@student.maastrichtuniversity.nl>
2023-01-16 16:40:14 +0100

    All the code that I wrote lately, RAVE is still not working properly. If someone can help it would be nice. If not, probably we will not have RAVE nor UCTRAVE
    Played around with the FitnessFunction, now some functions are more efficient.

--- src/main/java/montecarlo/UCT.java
+++ src/main/java/montecarlo/UCT.java
@@ -12,7 +12,9 @@ public class UCT {
      * @param node The node for which the upper confidence bound should be calculated.
      * @return A double >= 0.
      */
-    public double calculate(MonteCarloNode node) {
+    private double bias = 0.5;
+
+    public double calculateUCT(MonteCarloNode node) {
         final double c = Math.sqrt(2);
         double winCount = node.getWinCount();
         double visitCount = node.getVisitCount();
@@ -24,6 +26,21 @@ public class UCT {
             return ((winCount / visitCount) + (c * Math.sqrt((Math.log(visitCountParent)) / visitCount)));
         }
     }
+
+    public double calculateRAVE(MonteCarloNode node) {
+        final double c = Math.sqrt(2);
+        double winCount = node.getWinCount();
+        double visitCount = node.getVisitCount();
+        double visitCountParent = (node.getParent().getVisitCount());
+        double raveWin = node.getRaveWin();
+        double raveVisit = node.getRaveVisit();
+        if (visitCount == 0) {
+            return Integer.MAX_VALUE;
+        } else {
+            return ((winCount / visitCount) + (raveWin / raveVisit) + (c * Math.sqrt((Math.log(visitCountParent)) / visitCount)));
+        }
+    }
+
 
     /**
      * Finds the leaf node with the highest upper confidence bound.
@@ -39,7 +56,7 @@ public class UCT {
 
         while (iteration.getChildren().size() > 0) {
             for (MonteCarloNode n : iteration.getChildren()) {
-                double uct = calculate(n);
+                double uct = calculateUCT(n);
                 if (maxUCT < uct) {
                     bestNode = n;
                     maxUCT = uct;
@@ -50,4 +67,48 @@ public class UCT {
         }
         return bestNode;
     }
+    public MonteCarloNode getMaxRAVE(MonteCarloNode node) {
+        double maxUCT = -1;
+
+        MonteCarloNode bestNode = null;
+        MonteCarloNode iteration = node;
+
+        while (iteration.getChildren().size() > 0) {
+            for (MonteCarloNode n : iteration.getChildren()) {
+                double RAVE = calculateRAVE(n);
+                if (maxUCT < RAVE) {
+                    bestNode = n;
+                    maxUCT = RAVE;
+                }
+            }
+            iteration = bestNode;
+            maxUCT = -1;
+        }
+        return bestNode;
+    }
+
+
+
+    public MonteCarloNode getMaxUCTRAVE(MonteCarloNode node) {
+        double maxUCT = -1;
+        double maxRAVE = -1;
+        MonteCarloNode bestNode = null;
+        MonteCarloNode iteration = node;
+
+        while (iteration.getChildren().size() > 0) {
+            for (MonteCarloNode n : iteration.getChildren()) {
+                double uct = calculateUCT(n);
+                double rave = calculateRAVE(n);
+                double value = (1 - this.bias) * uct + this.bias * rave;
+                if (value > maxUCT) {
+                    bestNode = n;
+                    maxUCT = value;
+                }
+            }
+            iteration = bestNode;
+            maxUCT = -1;
+        }
+        return bestNode;
+    }
+
 }
